1f1ed40a1a93b163ba85ce328f7405c38b35bdf8
juliagrandury <juliagrandury@wustl.edu>
2022-04-21 14:57:03 -0400

    Fixing home page bug

--- projects.js
+++ projects.js
@@ -13,8 +13,15 @@ function openProject(id){
             window.open('https://juliagrandury.github.io/tigermoth2_website/', '_blank');
             break;
         case '2':
+            break;
         case '3':
-
+            break;
+        case '4':
+            break;
+        case '5':
+            break;
+        case '6':
+            break;
     }
 }
 
