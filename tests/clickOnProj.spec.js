23cf6d3b523cfaa913f1863c739481ab1a0806bc
juliagrandury <juliagrandury@wustl.edu>
2022-04-21 21:30:54 -0400

    tidying up

--- tests/clickOnProj.spec.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-//when a div is clicked, print the id
-describe('get id', function() {
-    it('gets the id of the div clicked', () => {
-        
-        console.log('the id retrieved was')
-    })
-})
